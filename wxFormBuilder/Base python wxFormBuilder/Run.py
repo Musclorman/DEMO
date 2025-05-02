74e25fb6e6ba128c76b97bedd9d4f46dd21558fe
Musclor13 <musclor13@noreply.localhost>
2025-05-02 22:42:27 +0200

    Téléverser les fichiers vers "wxFormBuilder/Base python wxFormBuilder"

---
+++ wxFormBuilder/Base python wxFormBuilder/Run.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import wx
+import wx.xrc
+
+import genwx
+
+
+compteclic= 0
+
+class MaFenetre(genwx.Fenetre):
+    def clicsurlebouton(self, event):
+        print("clic sur le bouton")
+        global compteclic
+        compteclic+=1
+
+app = wx.App()
+MaFenetre(None).Show()
+print("avant MainLoop")
+app.MainLoop()
+print("apres MainLoop et", compteclic, "clics")
+
